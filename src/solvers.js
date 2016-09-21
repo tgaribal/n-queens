c95a3fdb127eb219a94bff73615b98fbfd0db3ed
tgaribal <tgaribal@gmail.com>
2016-09-21 16:02:49 -0700

    Solve findNRookSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,26 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  //create a board that is nxn
+  var board = new Board({n: n});
+  var rows = board.rows();
+  var results = [];
+  //iterate over first row in the board
+  for (var i = 0; i < rows.length; i++) {
+    //iterate over array i's indexes
+    for (var j = 0; j < rows[i].length; j++) {
+      //set current location to 1
+      board.togglePiece(i, j);
+      //check row/col conflict if none then set that index = 1
+      if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
+        //then set index to 0
+        board.togglePiece(i, j);
+      } 
+    }
+    results.push(rows[i]);
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  return results;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
